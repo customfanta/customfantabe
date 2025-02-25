682ee6bf701f85409a884ed669fe7bdadc6b3524
Antonio Pellegrino <a.pellegrino@reply.it>
2025-02-25 22:12:33 +0100

    aggiunto log

--- src/main/java/it/customfanta/be/controller/EmptyController.java
+++ src/main/java/it/customfanta/be/controller/EmptyController.java
@@ -3,12 +3,17 @@ package it.customfanta.be.controller;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
+import java.util.logging.Logger;
+
 @RestController
 @CrossOrigin(originPatterns = "*", allowCredentials = "true", allowedHeaders = "*")
 public class EmptyController {
 
+    private static final Logger logger = Logger.getLogger(EmptyController.class.getName());
+
     @RequestMapping(method = RequestMethod.GET, value = "/test", produces = { "application/json" })
     public ResponseEntity<String> test() {
+        logger.info("API RICHIAMATA");
         return ResponseEntity.ok("Test OK");
     }
 }
