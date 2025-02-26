3d47a5c3db17945fda7906a0898e5ea0975d5011
Antonio Pellegrino <a.pellegrino@reply.it>
2025-02-26 08:33:39 +0100

    aggiunto health controller

---
+++ src/main/java/it/customfanta/be/controller/HealthController.java
@@ -0,0 +1,24 @@
+package it.customfanta.be.controller;
+
+import it.customfanta.be.service.UsersService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.logging.Logger;
+
+@RestController
+@CrossOrigin(originPatterns = "*", allowCredentials = "true", allowedHeaders = "*")
+public class HealthController {
+
+    private static final Logger logger = Logger.getLogger(HealthController.class.getName());
+
+    @RequestMapping(method = RequestMethod.GET, value = "/health", produces = { "application/json" })
+    public ResponseEntity<String> health() {
+        logger.info("Ricevuta chiamata /health");
+        return ResponseEntity.ok("Health Status: OK");
+    }
+}
