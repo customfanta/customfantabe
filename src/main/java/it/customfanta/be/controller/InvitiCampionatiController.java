2809500f1bac1b6d535595c506c5cdb3b630fced
Antonio Pellegrino <a.pellegrino@reply.it>
2025-03-29 11:36:14 +0100

    rifiuta invito

--- src/main/java/it/customfanta/be/controller/InvitiCampionatiController.java
+++ src/main/java/it/customfanta/be/controller/InvitiCampionatiController.java
@@ -191,4 +191,18 @@ public class InvitiCampionatiController extends BaseController {
         return ResponseEntity.ok(new Esito("OK"));
     }
 
+    @Operation(
+            responses = {
+                    @ApiResponse(responseCode = "200", content = {
+                            @Content(mediaType = "application/json", schema = @Schema(implementation = Esito.class))
+                    })
+            }
+    )
+    @RequestMapping(method = RequestMethod.GET, value = "/rifiuta-invito/{chiaveInvito}", produces = { "application/json" })
+    public ResponseEntity<Esito> rifiutaInvito(@PathVariable("chiaveInvito") String chiaveInvito) {
+        logger.info("RECEIVED GET /rifiuta-invito" + chiaveInvito);
+        invitiCampionatiRepository.deleteById(chiaveInvito);
+        return ResponseEntity.ok(new Esito("OK"));
+    }
+
 }
